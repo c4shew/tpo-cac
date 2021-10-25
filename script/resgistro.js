dce457b5a208a6b2192f2ebf84023b9bc4daa7b8
c4shew <constanzabianco@outlook.com>
2021-10-25 17:28:01 -0300

    comit no1 c/js

---
+++ script/resgistro.js
@@ -0,0 +1,22 @@
+
+var username = document.getElementById (username);
+
+if (username == null) {
+   alert ("El nombre de usuario debe contener al menos un carácter");
+   };
+
+var email = document.querySelector (email);
+
+let str = email;
+let arrmail = str.split('');
+
+arrmail.some (containsAtSign) ;
+
+function containsAtSign (element) { return element === @
+};
+
+if (containsAtSign == false){
+    alert ("Correo electrónico inválido");
+    };
+
+    
